96d4ed42a9f3ccdf8e6e215b93e705229b03175e
it22591302 <sandeepkahawatta9@gmail.com>
2024-06-17 14:52:38 +0530

    update seller single product 2

--- BACKEND/routes/sellerPartnership/sellerProducts.js
+++ BACKEND/routes/sellerPartnership/sellerProducts.js
@@ -92,7 +92,7 @@ router
       const product = await Product.findById(productId);
 
       const calculatedPrice =
-  ((100 - sellerProduct.price_margin) * product.price) /
+  ((100 - sellerProduct.price_margine) * product.price) /
   100;
 
       //merge product details
